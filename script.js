e1cada66db83c8534912fae52ee9215339551f01
unknown <hamim99bd@gmail.com>
2022-01-21 17:02:20 +0600

    number input

--- script.js
+++ script.js
@@ -4,4 +4,52 @@ generate_button.addEventListener('click',function(){
     // console.log(pin);
     const input_pin=document.getElementById('input-pin');
     input_pin.value=pin;
+});
+
+const provide_pin=document.getElementById('provide-pin');
+
+const one=document.getElementById(1);
+one.addEventListener('click',function(){
+    provide_pin.value+=1;
 })
+const two=document.getElementById(2);
+two.addEventListener('click',function(){
+    provide_pin.value+=2;
+})
+const three=document.getElementById(3);
+three.addEventListener('click',function(){
+    provide_pin.value+=3;
+})
+const four=document.getElementById(4);
+four.addEventListener('click',function(){
+    provide_pin.value+=4;
+})
+const five=document.getElementById(5);
+five.addEventListener('click',function(){
+    provide_pin.value+=5;
+})
+const six=document.getElementById(6);
+six.addEventListener('click',function(){
+    provide_pin.value+=6;
+})
+const seven=document.getElementById(7);
+seven.addEventListener('click',function(){
+    provide_pin.value+=7;
+})
+const eight=document.getElementById(8);
+eight.addEventListener('click',function(){
+    provide_pin.value+=8;
+})
+const nine=document.getElementById(9);
+nine.addEventListener('click',function(){
+    provide_pin.value+=9;
+})
+const zero=document.getElementById(0);
+zero.addEventListener('click',function(){
+    provide_pin.value+=0;
+})
+const clear=document.getElementById('clear');
+clear.addEventListener('click',function(){
+    provide_pin.value='';
+})
+
