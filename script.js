3b8cfc31bd9e5cbe55122fe0345cf3587ce5e84c
unknown <hamim99bd@gmail.com>
2022-01-21 17:39:20 +0600

    initially done

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
-const generate_button=document.getElementById('generate-button');
-var pin='';
-generate_button.addEventListener('click',function(){
+const generate_button = document.getElementById('generate-button');
+var pin = '';
+generate_button.addEventListener('click', function () {
     pin = Math.floor(1000 + Math.random() * 9000);
-   
-    const input_pin=document.getElementById('input-pin');
-    input_pin.value=pin;
+
+    const input_pin = document.getElementById('input-pin');
+    input_pin.value = pin;
 });
-const provide_pin=document.getElementById('provide-pin');
+const provide_pin = document.getElementById('provide-pin');
 
-const one=document.getElementById(1);
-one.addEventListener('click',function(){
-    provide_pin.value+=1;
+const one = document.getElementById(1);
+one.addEventListener('click', function () {
+    provide_pin.value += 1;
 })
-const two=document.getElementById(2);
-two.addEventListener('click',function(){
-    provide_pin.value+=2;
+const two = document.getElementById(2);
+two.addEventListener('click', function () {
+    provide_pin.value += 2;
 })
-const three=document.getElementById(3);
-three.addEventListener('click',function(){
-    provide_pin.value+=3;
+const three = document.getElementById(3);
+three.addEventListener('click', function () {
+    provide_pin.value += 3;
 })
-const four=document.getElementById(4);
-four.addEventListener('click',function(){
-    provide_pin.value+=4;
+const four = document.getElementById(4);
+four.addEventListener('click', function () {
+    provide_pin.value += 4;
 })
-const five=document.getElementById(5);
-five.addEventListener('click',function(){
-    provide_pin.value+=5;
+const five = document.getElementById(5);
+five.addEventListener('click', function () {
+    provide_pin.value += 5;
 })
-const six=document.getElementById(6);
-six.addEventListener('click',function(){
-    provide_pin.value+=6;
+const six = document.getElementById(6);
+six.addEventListener('click', function () {
+    provide_pin.value += 6;
 })
-const seven=document.getElementById(7);
-seven.addEventListener('click',function(){
-    provide_pin.value+=7;
+const seven = document.getElementById(7);
+seven.addEventListener('click', function () {
+    provide_pin.value += 7;
 })
-const eight=document.getElementById(8);
-eight.addEventListener('click',function(){
-    provide_pin.value+=8;
+const eight = document.getElementById(8);
+eight.addEventListener('click', function () {
+    provide_pin.value += 8;
 })
-const nine=document.getElementById(9);
-nine.addEventListener('click',function(){
-    provide_pin.value+=9;
+const nine = document.getElementById(9);
+nine.addEventListener('click', function () {
+    provide_pin.value += 9;
 })
-const zero=document.getElementById(0);
-zero.addEventListener('click',function(){
-    provide_pin.value+=0;
+const zero = document.getElementById(0);
+zero.addEventListener('click', function () {
+    provide_pin.value += 0;
 })
-const clear=document.getElementById('clear');
-clear.addEventListener('click',function(){
-    provide_pin.value='';
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function () {
+    provide_pin.value = '';
 })
 
-document.getElementById('submit-pin').addEventListener('click',function(){
-    const given_pin= parseInt(provide_pin.value);
-    if(given_pin==pin){
-        console.log("Correct");
+document.getElementById('submit-pin').addEventListener('click', function () {
+    const given_pin = parseInt(provide_pin.value);
+    if (given_pin == pin) {
+        alert("Correct");
+        document.getElementById('input-pin').value = '';
+        provide_pin.value = '';
+
+
     }
-    else{
-        console.log('incorrect');
+    else {
+        alert('incorrect');
+        provide_pin.value = '';
+
     }
 })
 
--- test.js
+++ test.js
@@ -1,5 +1,13 @@
+// while (1) {
+//     const pin = Math.floor(1000 + Math.random() * 9000);
+//     console.log(pin);
+//     if (pin == 9999) {
+//         break;
+//     }
+// }
+
 while (1) {
-    const pin = Math.floor(1000 + Math.random() * 9000);
+    const pin = Math.floor(Math.random()* 10000) ;
     console.log(pin);
     if (pin == 9999) {
         break;
